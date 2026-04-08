2274e89f1d355be8a32c3f9ff47b201e1b5a7721
yuetang1 <yue.tang.2@stonybrook.edu>
2026-04-07 23:15:31 -0400

    make hardcode an admin credential and user credential (need to setup presistent storage later)

--- inject-admin.sh
+++ inject-admin.sh
@@ -8,4 +8,8 @@ eg user create -p 'username=admin' -p 'firstname=admin' -p 'lastname=admin'
 
 eg scopes create admin
 
-eg credentials create -c admin -t key-auth -p "scopes=admin"
+eg credentials create -c admin -t key-auth -p "keyId=${ADMIN_API_KEY_ID}" -p "keySecret=${ADMIN_API_KEY_SECRET}" -p "scopes=admin"
+
+eg user create -p 'username=cse416-team-green' -p 'firstname=Hello' -p 'lastname=World'
+
+eg credentials create -c cse416-team-green -t key-auth -p "keyId=keyId" -p "keySecret=keySecret"
